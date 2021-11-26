d90d80d4f10f18693c11e4289e63d49647e4fb5f
yashp2 <yashp9145@gmail.com>
2021-11-27 03:57:19 +0530

    Updated Coding and instructions page

---
+++ coding_script.js
@@ -0,0 +1,16 @@
+document.getElementById('cfile')
+                  .addEventListener('change', function() {
+                    
+                  var fr=new FileReader();
+                  fr.onload=function(){
+                    //   document.getElementById('editor').textContent=fr.result;
+                    editor.setValue(fr.result);
+                    //   alert(fr.result);
+                  }
+                    
+                  fr.readAsText(this.files[0]);
+              })
+
+// var a=document.getElementById('Codelang').value;
+// alert(a);
+
